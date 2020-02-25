d5b009626039074c5a5d60dc11f33582796f941e
MK <masamichi.kagaya@gmail.com>
2020-02-25 09:26:46 +0900

    After Contentful plugin involved Before listing

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,5 +66,13 @@ module.exports = {
     "gatsby-plugin-offline",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
